c1c5d22dcb7dc3c010636e5a308ec4f23290064c
Ay00luwwa <ijidakinroayooluwa@gmail.com>
2025-02-05 13:30:42 +0100

    edit commit 1.2

--- number_classification/settings.py
+++ number_classification/settings.py
@@ -111,11 +111,11 @@ USE_I18N = True
 USE_TZ = True
 
 
-
+#static files configuration
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_DIRS = [
